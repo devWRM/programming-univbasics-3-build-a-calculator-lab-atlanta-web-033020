3f87b82255ecde29bbf1f1d7c1bcde5f94dbc3ee
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-25 02:54:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,9 +15,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-
+#  sum = (num1 / num2)
+#  remainder = sum - sum.mathfloor
+  return num2 % num1
 end
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
